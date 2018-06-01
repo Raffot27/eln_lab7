cd3c260898d304a72347a2dcf5f0572f3ca80328
Raffaele Tuzzo <37403051+Raffot27@users.noreply.github.com>
2018-06-01 16:00:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,40 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 
-#include "main_asm.h" /* interface to the assembly module */
-
 #define LED1 PTT_PTT4
 #define LED2 PTT_PTT5
 #define LED3 PTT_PTT6
 #define LED4 PTT_PTT7
 
-#define MYWAIT 100
+#define MYWAIT 2
 
 void main(void) 
 {
-  int i,j,k,z;
+  int i,j,tmp;
   
   /* put your own code here */
   DDRT = 0xF0; // set PTT4 to PTT7 as outputs
-  PTT = 0xF0; // set PTT4 to PTT7 to 1 (LED off)
+  PTT = 0xE0; // set PTT4 to PTT7 to 1 (LED off)
   
-  for(;;) 
+  
+  
+ 
+	//EnableInterrupts;
+
+
+  for(;;)
   {
-     for(i=0; i<MYWAIT; i++);
-       LED1= LED4 ^ 1;
-       for(j=0; j<MYWAIT; j++);
-         LED2= LED1;
-         for(k=0; k<MYWAIT; k++);
-           LED3=LED2;
-           for(z=0; z<MYWAIT; z++);
-             LED4=LED3;
-   
+     for (i=0; i<MYWAIT; i++)
+        for (j=0; j<MYWAIT; j++); //wait 1 sec
+            tmp=LED4;
+            LED4=LED3;
+            LED3=LED2;
+            LED2=LED1;
+            LED1=tmp;
+  
   
   
     _FEED_COP(); /* feeds the dog */
   } /* loop forever */
-    /* please make sure that you never leave main */
+  /* please make sure that you never leave main */
 }
